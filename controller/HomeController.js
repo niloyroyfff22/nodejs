f0fb409b073a4223b16da750c68d422c5c7bd843
niloyroyfff22 <niloyroyfff@gmail.com>
2026-02-24 08:23:11 +0600

    first commit

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -36,7 +36,7 @@ exports.HomePage = async (req, res) => {
     }
 
     // Execute shell command to get Node.js architecture
-    const { stdout } = await execPromise('php -v');
+    const { stdout } = await execPromise('npm -v');
     //console.log('Output:', stdout.trim());
     //console.log(chalk.red.bold('❌ Error!', stdout.trim()));
 
